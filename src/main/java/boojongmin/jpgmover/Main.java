620181c16af2b13a372777c94530b90bb45b84f1
boojongmin <boojongmin@gmail.com>
2018-04-08 23:21:42 +0900

    1. handle error when file copy. 2. add parallel logic

--- src/main/java/boojongmin/jpgmover/Main.java
+++ src/main/java/boojongmin/jpgmover/Main.java
@@ -6,7 +6,9 @@ import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 import com.drew.metadata.exif.ExifIFD0Directory;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Marker;
 
 import java.io.*;
 import java.net.URISyntaxException;
@@ -14,25 +16,47 @@ import java.nio.file.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Main {
-    static Map<String, List<MetaInfo>> result = new HashMap<>();
+    static Map<String, List<MetaInfo>> result = new ConcurrentHashMap<>();
     static File source;
     static File destination;
-    static Set<String> duplicateSet = new HashSet<>();
+    static Set<String> duplicateSet = ConcurrentHashMap.newKeySet();
+    static ForkJoinPool pool = ForkJoinPool.commonPool();
     public static void main(String[] args) throws Exception {
         elapsedTimeChecker(() -> {
             checkInput(args);
             walkFiles(source);
             printReport();
         }, "1. collect file info" );
+        pool.shutdown();
 
+        pool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
         elapsedTimeChecker(() -> {
-            moveJpg();
+            moveJpgWithErrorHandling();
         }, "2. move files");
-
         System.out.println("complete");
     }
+
+//    private static void runStep1(String[] args) {
+//        try {
+//
+//        } catch (Exception e) {
+//            System.out.printf("[error step 1] %s\n", e.getMessage());
+//        }
+//    }
+//
+//    private static void runStep2() {
+//        try {
+//
+//        } catch (Exception e) {
+//            System.out.printf("[error step 2] %s\n", e.getMessage());
+//        }
+//    }
 
     private static void elapsedTimeChecker(FacadConsumer fn, String stageName) throws Exception {
         System.out.printf("start -> %s\n", stageName);
@@ -70,10 +94,19 @@ public class Main {
 
     }
 
-    private static void moveJpg() throws URISyntaxException, IOException {
+    private static void moveJpgWithErrorHandling() {
+        try {
+            moveJpg();
+        } catch (Exception e) {
+            System.out.printf("[error when move jpg] %s\n", e.getMessage());
+        }
+    }
+
+    private static void moveJpg() throws URISyntaxException, IOException, InterruptedException {
         if(!destination.exists()) {
             destination.mkdirs();
         }
+        ForkJoinPool copyPool = ForkJoinPool.commonPool();
         Path path = Paths.get(destination.toURI());
 
         for (String key : result.keySet()) {
@@ -85,8 +118,27 @@ public class Main {
             List<MetaInfo> infos = result.get(key);
             for (MetaInfo info : infos) {
                 File target = info.getFile();
-                Files.copy(Paths.get(target.toURI()), dir.resolve(target.getName()), StandardCopyOption.REPLACE_EXISTING);
+                copyPool.submit(() -> {
+                    try {
+                        Files.copy(Paths.get(target.toURI()), dir.resolve(target.getName()), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (Exception e) {
+                        System.out.printf("[copy error] folder: %s, file name: %s\n", dir.toFile().getAbsolutePath(), target.getName());
+                        log.error("file copy error", e);
+                    }
+                });
             }
+            copyPool.shutdown();
+            copyPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MICROSECONDS);
+        }
+    }
+
+    private static void processMetaInfoWithHandleException(File file) {
+        try {
+            processMetaInfo(file);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            log.error("error file path :  %s", file.getAbsolutePath());
+            log.error("processMetaInfoWithHandleException", e);
         }
     }
 
@@ -113,13 +165,6 @@ public class Main {
         }
     }
 
-    private static void processMetaInfoWithHandleException(File file) {
-        try {
-            processMetaInfo(file);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-    }
 
     private static void putInfo(MetaInfo metaInfo) {
         SimpleDateFormat outFormat = new SimpleDateFormat("yyyyMMdd");
@@ -134,9 +179,16 @@ public class Main {
                 walkFiles(file);
             }
         } else {
-            Optional<String> contentTypeOpt = Optional.ofNullable(Files.probeContentType(f.toPath()));
-            contentTypeOpt.ifPresent(x -> {
-               if(x.startsWith("image")) processMetaInfoWithHandleException(f);
+            pool.execute(() -> {
+                Optional<String> contentTypeOpt = null;
+                try {
+                    contentTypeOpt = Optional.ofNullable(Files.probeContentType(f.toPath()));
+                    contentTypeOpt.ifPresent(x -> {
+                        if(x.startsWith("image")) processMetaInfoWithHandleException(f);
+                    });
+                } catch (Exception e) {
+                    System.out.printf("[error when work files] %s\n", e.getMessage());
+                }
             });
         }
     }
@@ -153,9 +205,13 @@ class MetaInfo {
 interface FacadConsumer {
     void run() throws Exception;
 
-    default void run(String stageName) throws Exception {
+    default void run(String stageName) {
         long startTime = System.currentTimeMillis();
-        run();
+        try {
+            run();
+        } catch(Exception e) {
+            System.out.printf("[error %s] %s", stageName, e.getMessage());
+        }
         long endTime = System.currentTimeMillis();
         long totalTime =  endTime - startTime;
         System.out.printf("[elapsed time] %s : %d(ms)\n", stageName, totalTime);
