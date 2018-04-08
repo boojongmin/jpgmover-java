82fb2668ddedbeef431e4863531a306178fdb192
boojongmin <boojongmin@gmail.com>
2018-04-09 00:41:18 +0900

    change console logging  from System.out to log4j because System.out is not thread safe.

--- src/main/java/boojongmin/jpgmover/Main.java
+++ src/main/java/boojongmin/jpgmover/Main.java
@@ -6,12 +6,11 @@ import com.drew.metadata.Metadata;
 import com.drew.metadata.Tag;
 import com.drew.metadata.exif.ExifIFD0Directory;
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.slf4j.Marker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,8 +19,11 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
 
-@Slf4j
 public class Main {
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger("Main");
+    static Logger errLogger = LoggerFactory.getLogger("Main");
+
+
     static Map<String, List<MetaInfo>> result = new ConcurrentHashMap<>();
     static File source;
     static File destination;
@@ -31,45 +33,30 @@ public class Main {
         elapsedTimeChecker(() -> {
             checkInput(args);
             walkFiles(source);
-            printReport();
         }, "1. collect file info" );
         pool.shutdown();
-
         pool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+
+        printReport();
+
         elapsedTimeChecker(() -> {
             moveJpgWithErrorHandling();
         }, "2. move files");
-        System.out.println("complete");
+        log.info("complete");
     }
 
-//    private static void runStep1(String[] args) {
-//        try {
-//
-//        } catch (Exception e) {
-//            System.out.printf("[error step 1] %s\n", e.getMessage());
-//        }
-//    }
-//
-//    private static void runStep2() {
-//        try {
-//
-//        } catch (Exception e) {
-//            System.out.printf("[error step 2] %s\n", e.getMessage());
-//        }
-//    }
-
     private static void elapsedTimeChecker(FacadConsumer fn, String stageName) throws Exception {
-        System.out.printf("start -> %s\n", stageName);
+        log.info(String.format("start -> %s\n", stageName));
         long startTime = System.currentTimeMillis();
         fn.run(stageName);
         long totalTime = System.currentTimeMillis() - startTime;
-        System.out.printf("end -> %s, elapsed time %d(ms)\n", stageName, totalTime);
+        log.info(String.format("end -> %s, elapsed time %d(ms)\n", stageName, totalTime));
     }
 
 
     private static void printReport() {
-        System.out.printf("total image count is %d\n", result.size());
-        System.out.printf("total image size is %.2f Mb\n",
+        log.info(String.format("total image count is %d\n", result.values().stream().map(x -> x.size()).reduce(0, Integer::sum)));
+        log.info(String.format("total image size is %.2f Mb\n",
             result.values().stream().map(x ->
                 x.stream()
                     .map(y -> y.getFile().length())
@@ -77,19 +64,18 @@ public class Main {
             )
             .reduce(0L, Long::sum)
             .doubleValue() / (1000 * 1000)
-        )
-        ;
+        ));
     }
 
     private static void checkInput(String[] args) {
         if(args.length != 2) {
-            System.out.println("[error] your input size is not 2.\nexit\n");
+            log.info("[error] your input size is not 2.\nexit\n");
             System.exit(0);
         }
         source = new File(args[0]);
         destination = new File(args[1]);
         if(!source.exists()) {
-            System.out.println("first argument is wrong. check image path");
+            log.info("first argument is wrong. check image path");
         }
 
     }
@@ -98,11 +84,13 @@ public class Main {
         try {
             moveJpg();
         } catch (Exception e) {
-            System.out.printf("[error when move jpg] %s\n", e.getMessage());
+            log.info(String.format("[error when move jpg] %s\n", e.getMessage()));
+            errLogger.error(String.format("[error when move jpg] %s\n", e.getMessage()), e);
+
         }
     }
 
-    private static void moveJpg() throws URISyntaxException, IOException, InterruptedException {
+    private static void moveJpg() throws InterruptedException {
         if(!destination.exists()) {
             destination.mkdirs();
         }
@@ -122,8 +110,8 @@ public class Main {
                     try {
                         Files.copy(Paths.get(target.toURI()), dir.resolve(target.getName()), StandardCopyOption.REPLACE_EXISTING);
                     } catch (Exception e) {
-                        System.out.printf("[copy error] folder: %s, file name: %s\n", dir.toFile().getAbsolutePath(), target.getName());
-                        log.error("file copy error", e);
+                        errLogger.info(String.format("[copy error] folder: %s, file name: %s\n", dir.toFile().getAbsolutePath(), target.getName()));
+                        errLogger.error("file copy error", e);
                     }
                 });
             }
@@ -136,9 +124,10 @@ public class Main {
         try {
             processMetaInfo(file);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            log.error("error file path :  %s", file.getAbsolutePath());
-            log.error("processMetaInfoWithHandleException", e);
+            log.info(e.getMessage());
+            log.info(String.format("error file path :  %s", file.getAbsolutePath()));
+            errLogger.error(String.format("error file path :  %s", file.getAbsolutePath()));
+            errLogger.error("processMetaInfoWithHandleException", e);
         }
     }
 
@@ -180,14 +169,13 @@ public class Main {
             }
         } else {
             pool.execute(() -> {
-                Optional<String> contentTypeOpt = null;
                 try {
-                    contentTypeOpt = Optional.ofNullable(Files.probeContentType(f.toPath()));
+                    Optional<String> contentTypeOpt = Optional.ofNullable(Files.probeContentType(f.toPath()));
                     contentTypeOpt.ifPresent(x -> {
                         if(x.startsWith("image")) processMetaInfoWithHandleException(f);
                     });
                 } catch (Exception e) {
-                    System.out.printf("[error when work files] %s\n", e.getMessage());
+                    errLogger.error(String.format("[error when work files] %s\n", e.getMessage()));
                 }
             });
         }
@@ -210,10 +198,10 @@ interface FacadConsumer {
         try {
             run();
         } catch(Exception e) {
-            System.out.printf("[error %s] %s", stageName, e.getMessage());
+            Main.log.info(String.format("[error %s] %s", stageName, e.getMessage()));
         }
         long endTime = System.currentTimeMillis();
         long totalTime =  endTime - startTime;
-        System.out.printf("[elapsed time] %s : %d(ms)\n", stageName, totalTime);
+        Main.log.info(String.format("[elapsed time] %s : %d(ms)\n", stageName, totalTime));
     }
 }
